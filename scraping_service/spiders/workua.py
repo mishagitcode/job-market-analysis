abf781c655d6a3de4fc6dd6224ea5bb162e68604
Misha Zubritsky <zubritsky.misha@gmail.com>
2025-12-03 00:43:38 +0200

    Fix parse_job_page_info method

--- scraping_service/spiders/workua.py
+++ scraping_service/spiders/workua.py
@@ -24,43 +24,27 @@ class WorkuaSpider(scrapy.Spider):
                 meta={"specialization": specialization}
             )
 
-        # next_page_disabled = response.css("li.add-left-default::attr(class)").get().split(" ")[1]
-        # if next_page_disabled != "disabled":
-        #     next_page_number = int(response.css(".pagination > .active > span::text").get()) + 1
-        #     next_page = response.urljoin(f"?page={next_page_number}")
-        #     yield response.follow(next_page, callback=self.parse)
+        next_page_disabled = response.css("li.add-left-default::attr(class)").get().split(" ")[1]
+        if next_page_disabled != "disabled":
+            next_page_number = int(response.css(".pagination > .active > span::text").get()) + 1
+            next_page = response.urljoin(f"?page={next_page_number}")
+            yield response.follow(next_page, callback=self.parse)
 
     def parse_job_page_info(self, response: Response):
         specialization = response.meta["specialization"]
         title = response.css("h1.my-0::text").get()
+
         company = response.css(
             "ul.list-unstyled > li > a.inline > span.strong-500::text"
         ).get()
 
-        salary = response.css(
-            "ul.list-unstyled > li > span.strong-500::text"
-        ).get()
-        if salary:
-            salary = salary.replace("\u202f", "").replace("\u2009", "").replace(" грн", "")
-        else:
-            salary = 0
-
-        if salary == 0:
-            experience = response.css(
-                "ul.list-unstyled > li.text-indent"
-            ).xpath(
-                "normalize-space(string())"
-            ).getall()[2].split(" від ")[1].replace(" років.", "")
-        else:
-            experience = response.css(
-                "ul.list-unstyled > li.text-indent"
-            ).xpath(
-                "normalize-space(string())"
-            ).getall()[3].split(" від ")[1].replace(" років.", "")
-
-        skills = response.css("ul.flex > li > span::text").getall()
-
-        remote, office, location = self.work_style_and_location_info(response)
+        skills = self.get_skills_info(response)
+        salary = self.get_salary_info(response)
+        years_of_experience = self.get_experience_info(response, salary)
+        remote, office, location = self.get_work_style_and_location_info(
+            response,
+            salary
+        )
 
         yield {
             "specialization": specialization,
@@ -70,14 +54,74 @@ class WorkuaSpider(scrapy.Spider):
             "location": location,
             "office": office,
             "remote": remote,
-            "min_experience_years": experience,
+            "years_of_experience": years_of_experience,
             "skills": skills
         }
 
-    def work_style_and_location_info(self, response: Response):
+    def get_skills_info(self, response: Response) -> int | list[str]:
+        skills = response.css("ul.flex > li > span::text").getall()
+        if not skills:
+            return 0
+        else:
+            return skills
+
+    def get_salary_info(self, response: Response) -> int | str:
+        salary = response.css(
+            "ul.list-unstyled > li > span.strong-500::text"
+        ).get()
+
+        if salary:
+            return (salary.replace("\u202f", "")
+                    .replace("\u2009", "").replace(" грн", ""))
+        else:
+            return 0
+
+    def get_experience_info(
+            self,
+            response: Response,
+            salary: int | None
+    ) -> str | int:
+        i = 0
+        if salary == 0:
+            i = 2
+        else:
+            i = 3
+
+        words = response.css("ul.list-unstyled > li.text-indent").xpath(
+            "normalize-space(string())"
+        ).getall()[i].split(" ")
+
+        years_of_experience = "".join([
+            word
+            for word in words
+            if word.isdigit()
+        ])
+        if not years_of_experience:
+            return 0
+        else:
+            return years_of_experience
+
+    def get_work_style_and_location_info(
+            self,
+            response: Response,
+            salary: str | int
+    ) -> (int, int, int):
+        i = 0
+        check_for_hourly_salary = response.css(
+            "ul.list-unstyled > li.text-indent > span::attr(title)"
+        ).get()
+
+        if salary == 0:
+            if check_for_hourly_salary == "Зарплата":
+                i = 2
+            else:
+                i = 1
+        else:
+            i = 2
+
         work_style = response.css(
             "ul.list-unstyled > li.text-indent"
-        ).xpath("normalize-space(string())").getall()[2]
+        ).xpath("normalize-space(string())").getall()[i]
 
         if work_style == "Дистанційна робота":
             remote = 1
@@ -87,5 +131,5 @@ class WorkuaSpider(scrapy.Spider):
         else:
             remote = 0
             office = 1
-            location = work_style
+            location = work_style.split(",")[0]
             return remote, office, location
