e16780b8ae7ae10536697857502346b35f3f1753
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-01-13 15:04:23 +0200

    Implement skills column util

--- data_cleaning_service/utils/utils_workua.py
+++ data_cleaning_service/utils/utils_workua.py
@@ -30,3 +30,12 @@ def parse_salary(salary_str: str) -> (int, int, int):
         return min_val, max_val, avg_val
     except (ValueError, IndexError):
         return np.nan, np.nan, np.nan
+
+
+def clean_and_normalize_skills(skills_str):
+    if pd.isna(skills_str) or str(skills_str).strip() in ['0', '']:
+        return ""
+
+    skills_list = str(skills_str).split(',')
+    cleaned_skills = {s.strip().capitalize() for s in skills_list if s.strip()}
+    return ",".join(sorted(cleaned_skills))
